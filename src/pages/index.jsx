31a2bbe8e42cda9f570527f86d4075d874dfc246
Luiz Henrique <luizboaventura24@gmail.com>
2025-04-25 13:53:04 -0300

    feat: configure firebase

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,37 @@
-export default function Home () {
+import { useEffect } from "react";
+import { auth } from "../lib/firebase/firebase.config";
+import { signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+
+export default function Home() {
+  useEffect(() => {
+    const checkRedirectResult = async () => {
+      try {
+        const result = await getRedirectResult(auth);
+
+        if (result) {
+          // Usuário logado com sucesso
+          console.log("Usuário:", result.user);
+          console.log("Token:", result._tokenResponse);
+        } else {
+          console.log("Nenhum resultado de login (normal se for primeira vez).");
+        }
+      } catch (error) {
+        console.error("Erro ao obter resultado de redirect:", error);
+      }
+    };
+
+    checkRedirectResult();
+  }, []);
+  
+  const handleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to the Home Page</h1>
+    <div style={{ padding: "2rem" }}>
+      <h1>Logar com Google</h1>
+      <button onClick={handleLogin}>Logar</button>
     </div>
   );
 }
