dd697f688f6b821de9b169bf71cace6bf6c2037b
Luiz Henrique <luizboaventura24@gmail.com>
2025-11-01 13:57:02 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.join(__dirname, '../../')
+  outputFileTracingRoot: path.join(__dirname)
 };
 export default nextConfig;
