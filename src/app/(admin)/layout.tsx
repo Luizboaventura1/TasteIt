cb4dc79887819b8d9cd5d560457c7a66e87e511d
Luiz Henrique <luizboaventura24@gmail.com>
2026-02-01 16:30:18 -0300

    feat: wrap admin layout with ClientLayout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,13 @@
 import DefaultFooter from "@/components/layouts/DefaultFooter";
 import MainNavbar from "@/components/layouts/MainNavbar";
+import ClientLayout from "@/components/features/ClientLayout";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ClientLayout>
       <MainNavbar />
       <main>{children}</main>
       <DefaultFooter />
-    </>
+    </ClientLayout>
   );
 }
